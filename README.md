3e4cecbe9080d45380c9344b0c35af164f345265
christopheblum67 <christopheblum67@gmail.com>
2026-08-29 08:22:29 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Orbit-3d-flutter
+﻿# 🪐 ORBIT 3D
+https://github.com/christopheblum67/orbit-3d
+
+## Mode Test Panels
+* Pro : TEST-PANEL-PRO-2026
+* Free : TEST-PANEL-FREE-2026
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+echo '=== 1. COMPILATION RUST FFI ==='
+cd native
+cargo ndk -t arm64-v8a -t armeabi-v7a -o ../android/app/src/main/jniLibs build --release
+cd ..
+echo '=== 2. BUILD FLUTTER APK ==='
+flutter clean
+flutter pub get
+flutter build apk --release --target-platform android-arm64
