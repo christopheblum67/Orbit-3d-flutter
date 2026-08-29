26b0b8532fca4e7bae49720cc21f987f682f50d2
christopheblum67 <christopheblum67@gmail.com>
2026-08-29 15:49:24 +0200

    Passage de MainActivity vers Android v2 embedding et nettoyage workflow

--- android/app/src/main/kotlin/com/example/orbit_3d/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/orbit_3d/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.orbit_3d
+﻿package com.example.orbit_3d
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity()
