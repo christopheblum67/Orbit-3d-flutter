cc1dee1a16ed7e378f3baff9314da30d7cce7e27
christopheblum67 <christopheblum67@gmail.com>
2026-08-29 15:09:41 +0200

    Validation AndroidManifest hybride TV et Mobile

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,11 +22,10 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 plugins {
-    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
-    id "com.android.application" version "8.3.2" apply false
-    id "org.jetbrains.kotlin.android" version "1.9.24" apply false
+    id("dev.flutter.flutter-plugin-loader") version("1.0.0")
+    id("com.android.application") version("8.3.2") apply(false)
+    id("org.jetbrains.kotlin.android") version("1.9.24") apply(false)
 }
 
 include ":app"
